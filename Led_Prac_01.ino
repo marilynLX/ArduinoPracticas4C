2dccff8f1194d7409c9a61ce914b4b2651c1e771
marilynLX <179334509+marilynLX@users.noreply.github.com>
2024-11-03 12:11:31 -0600

    Update Led_Prac_01.ino

--- Led_Prac_01.ino
+++ Led_Prac_01.ino
@@ -1,10 +1,13 @@
+//declaramos la variable del led en el puerto 13
 int led = 13;
 void setup() {
   // put your setup code here, to run once:
+  //indicamos la salida de datos 
 pinMode (led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //indicamos que se prenda el led por tiempo indefinido
 digitalWrite (led, HIGH);
 }
